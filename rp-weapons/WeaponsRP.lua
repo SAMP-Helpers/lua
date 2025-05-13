1d9eb22da0313c608f05454014da0c25f120b5fe
MTGMODS <bogdan.mtg@gmail.com>
2025-05-13 05:22:41 +0300

    Update Files

--- rp-weapons/WeaponsRP.lua
+++ rp-weapons/WeaponsRP.lua
@@ -1,221 +1,253 @@
-local sex = 'Мужчина'
--- Если вам нужны женские отыгровки то смените на 'Женщина'
+local message_color = 0x009EFF
+local message_color_hex = '{009EFF}'
+    
+local path = getWorkingDirectory():gsub('\\','/') .. "/config/RP_Weapon.json"
+local config = {
+    settings = {
+        enable = true
+    },
+    player_info = {
+        sex = 'Мужчина' -- или Женщина
+    },
+	rpgun = {
+		name = 'RP Guns',
+		path = "/RP Guns.json",
+		data = {
+            rp_guns = {
+                [0] = {name = 'кулаки', enable = true, rpTake = 2},
+                [1] = {name = 'кастеты', enable = true, rpTake = 2},
+                [2] = {name = 'клюшку для гольфа', enable = true, rpTake = 1},
+                [3] = {name = 'дубинку', enable = true, rpTake = 3},
+                [4] = {name = 'острый нож', enable = true, rpTake = 3},
+                [5] = {name = 'биту', enable = true, rpTake = 1},
+                [6] = {name = 'лопату', enable = true, rpTake = 1},
+                [7] = {name = 'кий', enable = true, rpTake = 1},
+                [8] = {name = 'катану', enable = true, rpTake = 1},
+                [9] = {name = 'бензопилу', enable = true, rpTake = 1},
+                [10] = {name = 'дидло', enable = true, rpTake = 2},
+                [11] = {name = 'дидло', enable = true, rpTake = 2},
+                [12] = {name = 'вибратор', enable = true, rpTake = 2},
+                [13] = {name = 'вибратор', enable = true, rpTake = 2},
+                [14] = {name = 'букет цветов', enable = true, rpTake = 1},
+                [15] = {name = 'трость', enable = true, rpTake = 1},
+                [16] = {name = 'осколочную гранату', enable = true, rpTake = 3},
+                [17] = {name = 'дымовую гранату', enable = true, rpTake = 3},
+                [18] = {name = 'коктейль Молотова', enable = true, rpTake = 3},
+                [22] = {name = 'пистолет Colt45', enable = true, rpTake = 4},
+                [23] = {name = "электрошокер Taser-X26P", enable = true, rpTake = 4},
+                [24] = {name = 'пистолет Desert Eagle', enable = true, rpTake = 4},
+                [25] = {name = 'дробовик', enable = true, rpTake = 1},
+                [26] = {name = 'обрез', enable = true, rpTake = 1},
+                [27] = {name = 'улучшенный обрез', enable = true, rpTake = 1},
+                [28] = {name = 'пистолет-пулемёт Micro Uzi', enable = true, rpTake = 4},
+                [29] = {name = 'пистолет-пулемёт MP5', enable = true, rpTake = 4},
+                [30] = {name = 'автомат AK-47', enable = true, rpTake = 1},
+                [31] = {name = 'автомат M4', enable = true, rpTake = 1},
+                [32] = {name = 'пистолет-пулемёт Tec-9', enable = true, rpTake = 4},
+                [33] = {name = 'винтовку Rifle', enable = true, rpTake = 1},
+                [34] = {name = 'снайперскую винтовку Rifle', enable = true, rpTake = 1},
+                [35] = {name = 'ручную противотанковую ракету', enable = true, rpTake = 1},
+                [36] = {name = 'устройство для запуска ракет', enable = true, rpTake = 1},
+                [37] = {name = 'огнемёт', enable = true, rpTake = 1},
+                [38] = {name = 'миниган', enable = true, rpTake = 1},
+                [39] = {name = 'динамит', enable = true, rpTake = 3},
+                [40] = {name = 'детонатор', enable = true, rpTake = 3},
+                [41] = {name = 'перцовый балончик', enable = true, rpTake = 2},
+                [42] = {name = 'огнетушитель', enable = true, rpTake = 1},
+                [43] = {name = 'фотоапарат', enable = true, rpTake = 2},
+                [44] = {name = 'прибор ночного видения', enable = true, rpTake = 2},
+                [45] = {name = 'тепловизор', enable = true, rpTake = 2},
+                [46] = {name = 'ручной парашут', enable = true, rpTake = 1},
+                -- gta sa damage reason
+                [49] = {name = 'автомобиль', rpTake = 1},
+                [50] = {name = 'лопасти вертолёта', rpTake = 1},
+                [51] = {name = 'бомбу', rpTake = 1},
+                [54] = {name = 'коллизию', rpTake = 1},
+                -- ARZ Custom Guns
+                [71] = {name = 'пистолет Desert Eagle Steel', enable = true, rpTake = 4},
+                [72] = {name = 'пистолет Desert Eagle Gold', enable = true, rpTake = 4},
+                [73] = {name = 'пистолет Glock Gradient', enable = true, rpTake = 4},
+                [74] = {name = 'пистолет Desert Eagle Flame', enable = true, rpTake = 4},
+                [75] = {name = 'пистолет Python Royal', enable = true, rpTake = 4},
+                [76] = {name = 'пистолет Python Silver', enable = true, rpTake = 4},
+                [77] = {name = 'автомат AK-47 Roses', enable = true, rpTake = 1},
+                [78] = {name = 'автомат AK-47 Gold', enable = true, rpTake = 1},
+                [79] = {name = 'пулемёт M249 Graffiti', enable = true, rpTake = 1},
+                [80] = {name = 'золотую Сайгу', enable = true, rpTake = 1},
+                [81] = {name = 'пистолет-пулемёт Standart', enable = true, rpTake = 4},
+                [82] = {name = 'пулемёт M249', enable = true, rpTake = 1},
+                [83] = {name = 'пистолет-пулемёт Skorp', enable = true, rpTake = 4},
+                [84] = {name = 'автомат AKS-74 камуфляжный', enable = true, rpTake = 1},
+                [85] = {name = 'автомат AK-47 камуфляжный', enable = true, rpTake = 1},
+                [86] = {name = 'дробовик Rebecca', enable = true, rpTake = 1},
+                [87] = {name = 'портальную пушку', enable = true, rpTake = 1},
+                [88] = {name = 'ледяной меч', enable = true, rpTake = 1},
+                [89] = {name = 'портальную пушку', enable = true, rpTake = 4},
+                [90] = {name = 'оглушающую гранату', enable = true, rpTake = 3},
+                [91] = {name = 'ослепляющую гранату', enable = true, rpTake = 3},
+                [92] = {name = 'снайперскую винтовку McMillian TAC-50', enable = true, rpTake = 1},
+                [93] = {name = 'оглушающий пистолет', enable = true, rpTake = 4}
+            },
+            rpTakeNames = {
+                [1] = {"из-за спины", "за спину"},
+                [2] = {"из кармана", "в карман"},
+                [3] = {"из пояса", "на пояс"},
+                [4] = {"из кобуры", "в кобуру"}
+            },
+            gunActions = {
+                on = {},
+                off = {},
+                partOn = {},
+                partOff = {}
+            },
+            oldGun = nil,
+            nowGun = 0
+        }
+	},
 
-local weapons = {
-	FIST = 0,
-	BRASSKNUCKLES = 1,
-	GOLFCLUB = 2,
-	NIGHTSTICK = 3,
-	KNIFE = 4,
-	BASEBALLBAT = 5,
-	SHOVEL = 6,
-	POOLCUE = 7,
-	KATANA = 8,
-	CHAINSAW = 9,
-	PURPLEDILDO = 10,
-	WHITEDILDO = 11,
-	WHITEVIBRATOR = 12,
-	SILVERVIBRATOR = 13,
-	FLOWERS = 14,
-	CANE = 15,
-	GRENADE = 16,
-	TEARGAS = 17,
-	MOLOTOV = 18,
-	COLT45 = 22,
-	SILENCED = 23,
-	DESERTEAGLE = 24,
-	SHOTGUN = 25,
-	SAWNOFFSHOTGUN = 26,
-	COMBATSHOTGUN = 27,
-	UZI = 28,
-	MP5 = 29,
-	AK47 = 30,
-	M4 = 31,
-	TEC9 = 32,
-	RIFLE = 33,
-	SNIPERRIFLE = 34,
-	ROCKETLAUNCHER = 35,
-	HEATSEEKER = 36,
-	FLAMETHROWER = 37,
-	MINIGUN = 38,
-	SATCHELCHARGE = 39,
-	DETONATOR = 40,
-	SPRAYCAN = 41,
-	FIREEXTINGUISHER = 42,
-	CAMERA = 43,
-	NIGHTVISION = 44,
-	THERMALVISION = 45,
-	PARACHUTE = 46,
-	WEAPON_VEHICLE = 49,
-	HELI = 50,
-	BOMB = 51,
-	COLLISION = 54,
-	-- ARZ CUSTOM GUN
-	DEAGLE_STEEL = 71,
-	DEAGLE_GOLD = 72,
-	GLOCK_GRADIENT = 73,
-	DEAGLE_FLAME = 74,
-	PYTHON_ROYAL = 75,
-	PYTHON_SILVER = 76,
-	AK47_ROSES = 77,
-	AK47_GOLD = 78,
-	M249_GRAFFITI = 79,
-	SAIGA_GOLD = 80,
-	PPSH_STANDART = 81,
-	M249_STANDART = 82,
-	SKORP_STANDART = 83,
-	AKS74_CAMOUFLAGE1 = 84,
-	AK47_CAMOUFLAGE1 = 85,
-	REBECCA_SHOTGUN = 86,
-	OBJ58_PORTALGUN = 87,
-	ICE_SWORD = 88,
-	PORTALGUN = 89,
-	SOUND_GRENADE = 90,
-	EYE_GRENADE = 91,
-	MCMILLIAN_TAC50 = 92
 }
-local id = weapons
-weapons.names = {
-	[id.FIST] = 'кулаки',
-	[id.BRASSKNUCKLES] = 'кастеты',
-	[id.GOLFCLUB] = 'клюшку для гольфа',
-	[id.NIGHTSTICK] = 'дубинку',
-	[id.KNIFE] = 'острый нож',
-	[id.BASEBALLBAT] = 'биту',
-	[id.SHOVEL] = 'лопату',
-	[id.POOLCUE] = 'кий',
-	[id.KATANA] = 'катану',
-	[id.CHAINSAW] = 'бензопилу',
-	[id.PURPLEDILDO] = 'дидло',
-	[id.WHITEDILDO] = 'дидло',
-	[id.WHITEVIBRATOR] = 'вибратор',
-	[id.SILVERVIBRATOR] = 'вибратор',
-	[id.FLOWERS] = 'букет цветов',
-	[id.CANE] = 'трость',
-	[id.GRENADE] = 'осколочную гранату',
-	[id.TEARGAS] = 'дымовую гранату',
-	[id.MOLOTOV] = 'коктейль Молотова',
-	[id.COLT45] = 'пистолет Colt45',
-	[id.SILENCED] = "электрошокер Taser-X26P",
-	[id.DESERTEAGLE] = 'пистолет Desert Eagle',
-	[id.SHOTGUN] = 'дробовик',
-	[id.SAWNOFFSHOTGUN] = 'обрез',
-	[id.COMBATSHOTGUN] = 'улучшенный обрез',
-	[id.UZI] = 'пистолет-пулемёт Micro Uzi',
-	[id.MP5] = 'пистолет-пулемёт MP5',
-	[id.AK47] = 'автомат AK-47',
-	[id.M4] = 'автомат M4',
-	[id.TEC9] = 'пистолет-пулемёт Tec-9',
-	[id.RIFLE] = 'винтовку Rifle',
-	[id.SNIPERRIFLE] = 'снайперскую винтовку Rifle',
-	[id.ROCKETLAUNCHER] = 'ручную противотанковую ракету',
-	[id.HEATSEEKER] = 'устройство для запуска ракет',
-	[id.FLAMETHROWER] = 'огнемёт',
-	[id.MINIGUN] = 'миниган',
-	[id.SATCHELCHARGE] = 'динамит',
-	[id.DETONATOR] = 'детонатор',
-	[id.SPRAYCAN] = 'перцовый баланчик',
-	[id.FIREEXTINGUISHER] = 'огнетушитель',
-	[id.CAMERA] = 'фотоапарат Canon',
-	[id.NIGHTVISION] = 'прибор ночного видения',
-	[id.THERMALVISION] = 'тепловизор',
-	[id.PARACHUTE] = 'ручной парашут',
-	[id.WEAPON_VEHICLE] = 'автомобиль',
-	[id.HELI] = 'лопасти вертолёта',
-	[id.BOMB] = 'взрыв',
-	[id.COLLISION] = 'коллизию',
-	-- ARZ LAUNCHER GUNS
-	[id.DEAGLE_STEEL] = 'пистолет Desert Eagle Steel',
-	[id.DEAGLE_GOLD] = 'пистолет Desert Eagle Gold',
-	[id.GLOCK_GRADIENT] = 'пистолет Glock',
-	[id.DEAGLE_FLAME] = 'пистолет Desert Eagle Flame',
-	[id.PYTHON_ROYAL] = 'пистолет Colt Python',
-	[id.PYTHON_SILVER] = 'пистолет Colt Python Silver',
-	[id.AK47_ROSES] = 'автомат AK-47 Roses',
-	[id.AK47_GOLD] = 'автомат AK-47 Gold',
-	[id.M249_GRAFFITI] = 'пулемёт M249 Graffiti',	
-	[id.SAIGA_GOLD] = 'золотую Сайгу',
-	[id.PPSH_STANDART] = 'пистолет-пулемёт Standart',
-	[id.M249_STANDART] = 'пулемёт M249',
-	[id.SKORP_STANDART] = 'пистолет-пулемёт Skorp',
-	[id.AKS74_CAMOUFLAGE1] = 'автомат AKS-74 камуфляжный',
-	[id.AK47_CAMOUFLAGE1] = 'автомат AK-47 камуфляжный',
-	[id.REBECCA_SHOTGUN] = 'дробовик Rebecca',
-	[id.OBJ58_PORTALGUN] = 'портальную пушку',
-	[id.PORTALGUN] = 'портальную пушку',
-	[id.ICE_SWORD] = 'ледяной меч',
-	[id.SOUND_GRENADE] = 'оглушающую граната',
-	[id.EYE_GRENADE] = 'ослепляющую граната',
-	[id.MCMILLIAN_TAC50] = 'снайперскую винтовку McMillian TAC-50'
-}
-function weapons.get_name(id) 
-	return weapons.names[id]
+function load()
+    if doesFileExist(path) then
+        local file, errstr = io.open(path, 'r')
+        if file then
+            local contents = file:read('*a')
+            file:close()
+            if #contents ~= 0 then
+                local result, loaded = pcall(decodeJson, contents)
+                if result then
+                    config = loaded
+                end
+            end
+        end
+    end
 end
-local gunOn = {}
-local gunOff = {}
-local gunPartOn = {}
-local gunPartOff = {}
-local oldGun = nil
-local nowGun = 0
-local rpTakeNames = {{"из-за спины", "за спину"}, {"из кармана", "в карман"}, {"из пояса", "на пояс"}, {"из кобуры", "в кобуру"}}
-local rpTake = {
-	[2]=1, [5]=1, [6]=1, [7]=1, [8]=1, [9]=1, [14]=1, [15]=1, [25]=1, [26]=1, [27]=1, [28]=1, [29]=1, [30]=1, [31]=1, [32]=1, [33]=1, [34]=1, [35]=1, [36]=1, [37]=1, [38]=1, [42]=1, [77]=1, [78]=1, [78]=1, [79]=1, [80]=1, [81]=1, [82]=1, [83]=1, [84]=1, [85]=1, [86]=1, [92]=1, [87]=1, [88]=1, [49]=1, [50]=1, [51]=1, [54]=1,
-	[1]=2, [4]=2, [10]=2, [11]=2, [12]=2, [13]=2, [41]=2, [43]=2, [44]=2, [45]=2, [46]=2,
-	[16]=3, [17]=3, [18]=3, [39]=3, [40]=3, [90]=3, [91]=3, [3]=3,
-	[22]=4, [23]=4, [24]=4, [71]=4, [72]=4, [73]=4, [74]=4, [75]=4, [76]=4, [89]=4,
-}
-for id, weapon in pairs(weapons.names) do
-	if (id == 3 or (id > 15 and id < 19) or (id == 90 or id == 91)) then -- 3 16 17 18 (for gunOn)
-		if sex == "Мужчина" then
-			gunOn[id] = 'снял'
-		elseif sex == "Женщина" then
-			gunOn[id] = 'снялa'
-		end
-	else
-		if sex == "Мужчина" then
-			gunOn[id] = 'достал'
-		elseif sex == "Женщина" then
-			gunOn[id] = 'досталa'
-		end
-	end
-	if (id == 3 or (id > 15 and id < 19) or (id > 38 and id < 41) or (id == 90 or id == 91)) then -- 3 16 17 18 39 40 (for gunOff)
-		if sex == "Мужчина" then
-			gunOff[id] = 'повесил'
-		elseif sex == "Женщина" then
-			gunOff[id] = 'повесилa'
-		end
-	else
-		if sex == "Мужчина" then
-			gunOff[id] = 'убрал'
-		elseif sex == "Женщина" then
-			gunOff[id] = 'убралa'
-		end
-	end
-	if id > 0 then
-		gunPartOn[id] = rpTakeNames[rpTake[id]][1]
-		gunPartOff[id] = rpTakeNames[rpTake[id]][2]
-	end
+function save()
+    local file, errstr = io.open(path, 'w')
+    if file then
+        local result, encoded = pcall(encodeJson, settings)
+        file:write(result and encoded or "")
+        file:close()
+    end
+end
+load()
+
+function init_guns()
+    for id, weapon in pairs(config.rpgun.data.rp_guns) do
+        local rpTakeType = config.rpgun.data.rpTakeNames[weapon.rpTake]
+        config.rpgun.data.gunActions.partOn[id] = rpTakeType[1]
+        config.rpgun.data.gunActions.partOff[id] = rpTakeType[2]
+        if id == 3 or (id > 15 and id < 19) or (id == 90 or id == 91) then
+            config.rpgun.data.gunActions.on[id] = (config.player_info.sex == "Женщина") and "сняла" or "снял"
+        else
+            config.rpgun.data.gunActions.on[id] = (config.player_info.sex == "Женщина") and "достала" or "достал"
+        end
+        if id == 3 or (id > 15 and id < 19) or (id > 38 and id < 41) or (id == 90 or id == 91) then
+            config.rpgun.data.gunActions.off[id] = (config.player_info.sex == "Женщина") and "повесила" or "повесил"
+        else
+            config.rpgun.data.gunActions.off[id] = (config.player_info.sex == "Женщина") and "убрала" or "убрал"
+        end
+    end
+end
+function get_name_weapon(id)
+    return config.rpgun.data.rp_guns[id] and config.rpgun.data.rp_guns[id].name or "оружие"
+end
+function isExistsWeapon(id)
+    return config.rpgun.data.rp_guns[id] ~= nil
+end
+function isEnableWeapon(id)
+    return config.rpgun.data.rp_guns[id] and (config.rpgun.data.rp_guns[id].enable or false)
+end
+function handleNewWeapon(weaponId)
+    sampAddChatMessage('[RP Weapon] {ffffff}Обнаружено новое оружие с ID ' .. message_color_hex .. weaponId .. '{ffffff}, даю ему имя "оружие" и расположение "спина".', message_color)
+    sampAddChatMessage('[RP Weapon] {ffffff}Изменить имя или расположение оружия вы можете в в файле:', message_color)
+    sampAddChatMessage(path, message_color)
+    config.rpgun.data.rp_guns[weaponId] = {name = "оружие", enable = true, rpTake = 1}
+    save()
+    init_guns()
+end
+function processWeaponChange(oldGun, nowGun)
+    if not config.rpgun.data.gunActions.off[oldGun] or not config.rpgun.data.gunActions.on[nowGun] then
+        return
+    end
+
+    local actions = config.rpgun.data.gunActions
+    local weaponNames = config.rpgun.data.rp_guns
+    
+    if oldGun == 0 and nowGun == 0 then
+        return
+    elseif oldGun == 0 and not isEnableWeapon(nowGun) then
+        return
+    elseif nowGun == 0 and not isEnableWeapon(oldGun) then
+        return
+    elseif not isEnableWeapon(oldGun) and isEnableWeapon(nowGun) then
+        sampSendChat(string.format("/me %s %s %s",
+            actions.on[nowGun],
+            weaponNames[nowGun].name,
+            actions.partOn[nowGun]
+        ))
+    elseif isEnableWeapon(oldGun) and not isEnableWeapon(nowGun) then
+        sampSendChat(string.format("/me %s %s %s",
+            actions.off[oldGun],
+            weaponNames[oldGun].name,
+            actions.partOff[oldGun]
+        ))
+    elseif oldGun == 0 then
+        sampSendChat(string.format("/me %s %s %s",
+            actions.on[nowGun],
+            weaponNames[nowGun].name,
+            actions.partOn[nowGun]
+        ))
+    elseif nowGun == 0 then
+        sampSendChat(string.format("/me %s %s %s",
+            actions.off[oldGun],
+            weaponNames[oldGun].name,
+            actions.partOff[oldGun]
+        ))
+    else
+        sampSendChat(string.format("/me %s %s %s, после чего %s %s %s",
+            actions.off[oldGun],
+            weaponNames[oldGun].name,
+            actions.partOff[oldGun],
+            actions.on[nowGun],
+            weaponNames[nowGun].name,
+            actions.partOn[nowGun]
+        ))
+    end
 end
 
 function main()
 
-	if not isSampLoaded() or not isSampfuncsLoaded() then return end
-    while not isSampAvailable() do wait(0) end 
+    if not isSampLoaded() or not isSampfuncsLoaded() then return end
+    while not isSampAvailable() do wait(0) end
 
-	while true do
-		wait(0)
+    init_guns()
 
-		if nowGun ~= getCurrentCharWeapon(PLAYER_PED) then
-			oldGun = nowGun
-			nowGun = getCurrentCharWeapon(PLAYER_PED)
-			if oldGun == 0 then
-				sampSendChat("/me " .. gunOn[nowGun] .. " " .. weapons.get_name(nowGun) .. " " .. gunPartOn[nowGun])
-			elseif nowGun == 0 then
-				sampSendChat("/me " .. gunOff[oldGun] .. " " .. weapons.get_name(oldGun) .. " " .. gunPartOff[oldGun])
-			else
-				sampSendChat("/me " .. gunOff[oldGun] .. " " .. weapons.get_name(oldGun) .. " " .. gunPartOff[oldGun] .. ", после чего " .. gunOn[nowGun] .. " " .. weapons.get_name(nowGun) .. " " .. gunPartOn[nowGun])
-			end
-		end
+    sampRegisterChatCommand('sex', function ()
+        config.player_info.sex = (config.player_info.sex == "Женщина") and "Мужчина" or "Женщина"
+        save()
+        init_guns()
+        sampAddChatMessage('[RP Weapon] {ffffff}' .. config.player_info.sex, message_color)
+    end)
 
-	end
+    while true do 
+        wait(0)
+        if config.settings.enable then
+            if config.rpgun.data.nowGun ~= getCurrentCharWeapon(PLAYER_PED) then
+                config.rpgun.data.oldGun = config.rpgun.data.nowGun
+                config.rpgun.data.nowGun = getCurrentCharWeapon(PLAYER_PED)
+
+                if not isExistsWeapon(config.rpgun.data.oldGun) then
+                    handleNewWeapon(config.rpgun.data.oldGun)
+                elseif not isExistsWeapon(config.rpgun.data.nowGun) then
+                    handleNewWeapon(config.rpgun.data.nowGun)
+                end
+
+                processWeaponChange(config.rpgun.data.oldGun, config.rpgun.data.nowGun)
+            end
+        end
+    end
+
 end
+
+
+
