58939ffea2f2bd39218154d8b97b1135ed909ea9
MisterTokenRU <doprega2009@gmail.com>
2025-08-30 23:37:11 +0300

    Delete car_snake_fix directory

--- car_snake_fix/CarSnakeFix.lua
+++
@@ -1,6 +0,0 @@
-function main()
-    if not isSampLoaded() or not isSampfuncsLoaded() then return end
-    while not isSampAvailable() do wait(0) end 
-    sampAddChatMessage('[INFO] {ffffff}Скрипт "Car Snake Fix" загружен и активирован! Автор: MTG MODS',0xffff0000)
-    writeMemory(0x6AC0F0, 2, 0x9090, true)
-end
